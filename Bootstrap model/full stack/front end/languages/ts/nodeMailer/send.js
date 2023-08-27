b8d99766894963b1de8e796ac118924b0253d0ce
HR9048 <139846245+HR9048@users.noreply.github.com>
2023-08-28 01:29:02 +0530

    eef

---
+++ Bootstrap model/full stack/front end/languages/ts/nodeMailer/send.js
@@ -0,0 +1,24 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+   service: "gmail",
+   auth: {
+      user: "raghojiharish@gmail.com",
+      pass: "ooxhmpzuflrjpqag"
+   }
+});
+
+const mailOptions = {
+   from: "raghojiharish@gmail.com",
+   to: "harishraghoji11@gmail.com",
+   subject: "Nodemailer Test",
+   html: "Test sending Gmail using Node JS"
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+   if(error){
+      console.log(error);
+   }else{
+      console.log("Email sent: " + info.response);
+   }
+});
